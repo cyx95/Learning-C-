fc9606710ea7b48e8b3256fedf7c1857b09e1b3c
Chen Ying Xin <chenyingxin92@gmail.com>
2022-09-30 14:20:00 -0400

    functions for numbers

--- workingWithNum.cpp
+++ workingWithNum.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -9,7 +10,13 @@ int main()
 
     wholeNum++;
 
-    cout << wholeNum;
+    cout << pow(2, 5) << endl; 
+    cout << sqrt(36) << endl;
+    cout << round(4.3) << endl; // rounds to nearest whole number
+    cout << ceil(4.1) << endl; //rounds up
+    cout << floor(4.8) << endl; //rounds down
+    cout << fmax(3, 10) << endl; //tell us which is the biggest integer passed
+    cout << fmin(3, 10) << endl; // tells us which is the smallest integer passed
 
     return 0;
 }
