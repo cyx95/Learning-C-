1868b2c027e800008b70020e56de96e1a62d22ee
Chen Ying Xin <chenyingxin92@gmail.com>
2022-09-30 13:06:54 -0400

    setting up c++

---
+++ helloworld.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    string characterName = "John";
+    int characterAge;
+    characterAge = 35;
+    
+    cout << "There once was a main named John" << endl;
+    cout << "He was 35 years old" << endl;
+    cout << "He liked he name John" << endl;
+    cout << "But did not like being 35" << endl;
+
+    return 0;
+}
