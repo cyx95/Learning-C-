21a9691cb3a743c905b414751718fd1c82a2b935
Chen Ying Xin <chenyingxin92@gmail.com>
2022-09-30 14:32:48 -0400

    learning how to get userinput for integer and char

---
+++ gettingUserInput.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{   
+    // cin is used for int or char
+    int age; // char age; 
+    cout << "Enter your age: ";
+    cin >> age;
+
+    cout << "You are " << age << " years old" << endl;
+
+    return 0;
+}
